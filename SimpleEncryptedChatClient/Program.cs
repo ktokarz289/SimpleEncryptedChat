d60529a77c5a4d7cd28d0e94ef9374f9ce18108a
= <kj.tokarz@gmail.com>
2018-04-05 20:30:50 -0400

    Set up projects

---
+++ SimpleEncryptedChatClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleEncryptedChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
